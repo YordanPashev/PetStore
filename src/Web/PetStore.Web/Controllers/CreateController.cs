bad95c3ac8d39ce567742fc64421e7dbac7e8d63
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-10-07 00:32:38 +0300

    Add CreateController and Index View

---
+++ src/Web/PetStore.Web/Controllers/CreateController.cs
@@ -0,0 +1,16 @@
+﻿namespace PetStore.Web.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    using PetStore.Common;
+
+    public class CreateController : Controller
+    {
+        [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
